82c8381f8fcc55649c88d3114f898ff6a78dff7c
Alex LaFroscia <alex@lafroscia.com>
2017-11-24 15:09:11 -0500

    Update demo to work correctly

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -5,14 +5,17 @@ var reporter = require('postcss-reporter');
 var retinaBgImg = require('../lib/postcss-plugin');
 
 gulp.task('default', function() {
-  return gulp.src('./styles/*.css')
-    .pipe(postcss([
-      retinaBgImg({
-        assetDirectory: resolve(__dirname, '../tests/fixtures')
-      }),
-      reporter({
-        clearMessages: true
-      })
-    ]))
+  return gulp
+    .src('./styles/*.css')
+    .pipe(
+      postcss([
+        retinaBgImg({
+          assetDirectory: resolve(__dirname, '../tests/fixtures')
+        }),
+        reporter({
+          clearMessages: true
+        })
+      ])
+    )
     .pipe(gulp.dest('./output'));
 });
